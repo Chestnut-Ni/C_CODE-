8b891b4b0eb0e4af5bb86d071aa507ab8fee1d59
Chestnut-Ni <362625418@qq.com>
2023-01-15 22:14:23 +0800

    模拟（二十）（二十一）

---
+++ 题库/题库/1.13.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	int a=0;
+//	char b=0;
+//	int c=0;
+//	char d=0;
+//	scanf("%d%c%d%c",&a,&b,&c,&d);
+//	printf("%d %c %d %c ",a,b,c,d);
+//	return 0;
+//}
+
+//union myun
+//{
+//	struct{int x,y,z;}u; 
+//	int k;
+//}a;
+//int main() 
+//{
+//	a.u.x=4;
+//	a.u.y=5;
+//	a.u.z=6;
+//	a.k=0; 
+//	printf("%d\n",a.u.x);
+//}
+
+//int main()
+//{
+//	printf("%f%",1.0/3);
+//}
+
+#include<math.h>
+//int main()
+//{
+//	long s=7654321;
+//	long t=0;
+//	int i=0;
+//	while(s)
+//	{
+//		t+=(s/10)%10*pow(10,i);
+//		i++;
+//		s=s/100;
+//	}
+//	printf("%ld",t);
+//}
+
+//int main()
+//{
+//	char ch = 'a';
+//	int k = 12;
+//	printf("%x,%o,", ch, ch, k);
+//	printf("k=%%d\n",k);
+//}
+
+//int main()
+//{
+//	char x=56;
+//	//x=x&056;
+//	printf("%d",x);
+//	//printf("%d %o",x,x);
+//}
